d51e4f404d3690f96a3f2e7535a869d98172026d
lam.bui <lam.bui@cloudgo.vn>
2025-01-31 10:35:27 +0700

    connect DB & install nodemon (dev)

--- README.md
+++ README.md
@@ -6,5 +6,12 @@
 yarn init
 yarn add sequelize mysql2   
 yarn add sequelize-cli --dev
+yarn add nodemon --dev
 
 yarn sequelize init
+
+
+
+# Tạo nhanh database từ config có sẵn
+> Hệ thống sẽ dựa theo thông tin DB từ file config.json để khởi tạo DB tương ứng
+yarn sequelize db:create 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { sequelize } = require("./models");
 
 const app = express();
 
@@ -11,6 +12,14 @@ const publicPathDir = path.join(__dirname, "./public");
 app.use(express.static(publicPathDir));
 
 // Listen connection event
-app.listen(3000, () => {
+app.listen(3000, async () => {
     console.log('App listening on http://localhost:3000!');
+
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } 
+    catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 });
