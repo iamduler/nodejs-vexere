6e33f4adb71bc60d06dd7e8f5d7a646163c2f481
lam.bui <lam.bui@cloudgo.vn>
2025-02-01 18:24:42 +0700

    Store image using multer (advanced)

--- routers/user.routers.js
+++ routers/user.routers.js
@@ -8,7 +8,18 @@ userRouter.post("/register", register);
 
 userRouter.post("/login", login);
 
-const upload = multer({ dest: "./uploads" });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // Hệ thống sẽ đứng từ file server.js để truy xuất tới folder /public. 
+        // Do file router được import & sử dụng trong file server.js
+        cb(null, "./public/images/avatar"); // Setup chỗ cần lưu file. 
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '_' + file.originalname); // Đặt lại tên cho file
+    }
+})
+
+const upload = multer({ storage: storage });
 userRouter.post('/upload-avatar', upload.single("avatar"), (req, res) => {
     res.send("Upload avatar...");
 })
